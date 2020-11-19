051946a9cdd883d31f793b370d86e9f4201787b9
Júlio Neves <juliocnevess@gmail.com>
2020-11-19 16:42:01 -0300

    Android Kotlin Fundamentals (AKF) Section 01.2.11

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.text.isDigitsOnly
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +14,23 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{ rollDice() }
+
+        val countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener{ countUp() }
     }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val result = resultText.text.toString().toIntOrNull() // tries to convert the text to a number
+
+        // if the text is not a number
+        if (result == null) {
+            resultText.text = 1.toString()
+        } else if (result != 6) {
+            resultText.text = (result + 1).toString()
+        }
+    }
+
 
     private fun rollDice() {
         val randomInt = (1..6).random()
