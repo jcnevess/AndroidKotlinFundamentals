53524844d32178d0cb72ddf2fc1d825af1609790
Júlio Neves <juliocnevess@gmail.com>
2020-11-19 17:43:30 -0300

    AKF 01.3.8 - Dice Roller

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,21 +6,29 @@ import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var diceImage : ImageView
+    lateinit var diceImage1 : ImageView
+    lateinit var diceImage2 : ImageView
     lateinit var rollButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        diceImage = findViewById(R.id.dice_image)
+        diceImage1 = findViewById(R.id.dice_image_1)
+        diceImage2 = findViewById(R.id.dice_image_2)
 
         rollButton = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{ rollDice() }
     }
 
     private fun rollDice() {
+        diceImage1.setImageResource(getDiceDrawableId())
+        diceImage2.setImageResource(getDiceDrawableId())
+    }
+
+    private fun getDiceDrawableId(): Int {
         val randomInt = (1..6).random()
+
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -29,7 +37,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
-        diceImage.setImageResource(drawableResource)
+        return drawableResource
     }
 }
