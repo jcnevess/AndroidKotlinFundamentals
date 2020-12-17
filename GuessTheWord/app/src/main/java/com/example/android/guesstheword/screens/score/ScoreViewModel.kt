c95df4602e3fd5e734c1b3a57cd1ba8e905112fe
Júlio Neves <juliocnevess@gmail.com>
2020-12-17 10:39:01 -0300

    AKF 05.2.9 GuessTheWord

--- GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -1,12 +1,30 @@
 package com.example.android.guesstheword.screens.score
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ScoreViewModel(finalScore: Int): ViewModel() {
-    var score = finalScore
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
+    // Using the observer pattern for a event performed after a button click is really needed?
+    // I'm doing it for the sake of exercising what I've learn, but it seems like an overkill.
+    private val _eventPlayAgain = MutableLiveData<Boolean>()
+    val eventPlayAgain: LiveData<Boolean>
+        get() = _eventPlayAgain
 
     init {
+        _score.value = finalScore
         Log.i("ScoreViewModel", "Final score is $finalScore")
+    }
+
+    fun onPlayAgain() {
+        _eventPlayAgain.value = true
+    }
+    fun onPlayAgainComplete() {
+        _eventPlayAgain.value = false
     }
 }
